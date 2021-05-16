a018b515fd2aef3052eceb6b821eb1772e52e4ab
NatalieMK9 <64727532+NatalieMK9@users.noreply.github.com>
2021-05-16 09:43:43 -0400

    Flying Spaceship Added

--- FinalProject/Assets/ChangingText.cs
+++ FinalProject/Assets/ChangingText.cs
@@ -20,29 +20,29 @@ public class ChangingText : MonoBehaviour
         timeRemaining -= Time.deltaTime;
         if (timerIsRunning)
         {
-            if (timeRemaining < 650 && timeRemaining > 640)
+            if (timeRemaining < 625 && timeRemaining > 620)
             {
                 ScreenText.text = "We're thrilled to have you onboard as our newest ensign!";
             }
-            if (timeRemaining < 640 && timeRemaining > 630)
+            if (timeRemaining < 620 && timeRemaining > 615)
             {
                 ScreenText.text = "Unfortunatley, you have managed to lock yourself in this room.";
             }
-            if (timeRemaining < 630 && timeRemaining > 620)
+            if (timeRemaining < 615 && timeRemaining > 610)
             {
                 ScreenText.text = "If you can't get out in 10 minutes, your superiors will be mad!";
             }
-            if (timeRemaining < 620 && timeRemaining > 610)
+            if (timeRemaining < 610 && timeRemaining > 605)
             {
                 ScreenText.text = "Solve the puzzles around the room to find a way out.";
             }
-            if (timeRemaining < 610 && timeRemaining > 600)
+            if (timeRemaining < 605 && timeRemaining > 600)
             {
                 ScreenText.text = "Good luck, ensign!";
             }
             if (timeRemaining < 600 && timeRemaining > 0)
             {
-                    ScreenText.text = string.Format("{0:N2}", timeRemaining);
+                ScreenText.text = string.Format("{0:N2}", timeRemaining);
             }
             //timeRemaining = 0;
             //timerIsRunning = false;
