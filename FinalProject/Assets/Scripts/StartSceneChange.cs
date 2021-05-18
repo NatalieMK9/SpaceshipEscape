d6044afa788c8b8af6008b3cdf5010c46044ad08
NatalieMK9 <64727532+NatalieMK9@users.noreply.github.com>
2021-05-18 18:35:52 -0400

    keycard to start

--- FinalProject/Assets/Scripts/StartSceneChange.cs
+++ FinalProject/Assets/Scripts/StartSceneChange.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartSceneChange : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //2
+        if (other.name == "DebitCard")
+        {
+            SceneManager.LoadScene("vrfinalspace", LoadSceneMode.Additive);
+        }
     }
 }
